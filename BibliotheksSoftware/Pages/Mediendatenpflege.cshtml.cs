dcc6b0d71cff7e969aa059bda441f15c7bff8974
Teresa Edinger <Edinger.Teresa@sus.htl-grieskirchen.at>
2022-04-08 15:24:52 +0200

    Mediendatenpflege Layout beginn Tabs

---
+++ BibliotheksSoftware/Pages/Mediendatenpflege.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BibliotheksSoftware.Pages.Mediendatenpflege
+{
+    public class TiteldatenTabModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

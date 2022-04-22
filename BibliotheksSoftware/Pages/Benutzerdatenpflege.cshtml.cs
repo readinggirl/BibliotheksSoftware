432198d004396bc656a33fd5cce955b4cebd5110
Zwilli <mikeyeyey@gmail.com>
2022-04-22 15:59:16 +0200

    UI arbeiten
    Startseite UI content fertig (design noch verbessern)
    Reservieren UI anfang
    Benutzerdatenpflege UI anfang

---
+++ BibliotheksSoftware/Pages/Benutzerdatenpflege.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BibliotheksSoftware.Pages.Benutzerdatenpflege
+{
+    public class TiteldatenTabModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

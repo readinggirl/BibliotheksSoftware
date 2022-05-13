e646c2fe59dc4250efb6111f3cc8171f4221addb
Zwilli <mikeyeyey@gmail.com>
2022-05-13 15:52:26 +0200

    Create BuchContext.cs

---
+++ BuchDB/BuchContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuchDB
+{
+    internal class BuchContext : DbContext
+    {
+        public const string dbPath = "D:\\Temp\\BuchDb.mdf";
+        public string connectionString = "";
+        public BuchContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected BuchContext()
+        {
+            connectionString = $"Server=(LocalDB)\\mssqllocaldb;attachdbfilename={dbPath}; integrated security = True; MultipleActiveResultSets=True";
+        }
+    }
+}

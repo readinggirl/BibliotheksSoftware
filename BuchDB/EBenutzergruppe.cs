74c1caf9527534755809d75c1731d0104b016022
Teresa Edinger <Edinger.Teresa@sus.htl-grieskirchen.at>
2022-05-13 15:49:28 +0200

    Enums erstellt

---
+++ BuchDB/EBenutzergruppe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuchDB;
+public enum EBenutzergruppe {
+    W, M, JM, JW, KM, KW, I
+}

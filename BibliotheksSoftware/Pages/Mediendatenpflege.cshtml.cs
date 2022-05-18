6e6d1846d88b809d44d0c1d9ae2b26e981f846e5
Teresa Edinger <Edinger.Teresa@sus.htl-grieskirchen.at>
2022-05-18 12:47:21 +0200

    jQuery probiert

--- BibliotheksSoftware/Pages/Mediendatenpflege.cshtml.cs
+++ BibliotheksSoftware/Pages/Mediendatenpflege.cshtml.cs
@@ -2,14 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 //[BindProperties]
-namespace BibliotheksSoftware.Pages.Mediendatenpflege
-{
-    public class MediendatenpflegeModel : PageModel
-    {
-        
-        public void OnGet()
-        {
-            
+namespace BibliotheksSoftware.Pages.Mediendatenpflege {
+    public class MediendatenpflegeModel : PageModel {
+
+        public void OnGet() {
+
         }
     }
 }
--- BibliotheksSoftware/wwwroot/js/benutzerdatenpflege.js
+++ BibliotheksSoftware/wwwroot/js/benutzerdatenpflege.js
@@ -1,10 +1,11 @@
-﻿$(_ => {
+﻿const bootstrap = require('bootstrap');
+$(_ => {
     console.log('JQuery ready');
 
     const baseUrl="https://localhost:5000";
     const url=`${baseUrl}/benutzer/id`;
 
-    $.post(url)
+    $.post(url);
      
 });
 

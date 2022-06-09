d39d0eda0e9ed48bee232db09f30dbc332d52827
Teresa Edinger <Edinger.Teresa@sus.htl-grieskirchen.at>
2022-06-09 18:34:53 +0200

    post demo

--- BibliotheksSoftware/wwwroot/js/mediendatenpflege.js
+++ BibliotheksSoftware/wwwroot/js/mediendatenpflege.js
@@ -1,3 +1,25 @@
 $(_ => {
-    console.log('alsvla');
+    console.log('JQuery ready');
+
+    const baseUrl = "https://localhost:5000";
+
+    $('.btn btn-new').on('click', _ => {
+
+        const medium = {
+            medienNummer: 1,
+            medienArt: 'asd',
+            systematik: 'jd',
+            isbn: '135789754321',
+            titel: 'myTitel',
+            verfasser: 'a cooler Verfasser',
+            jahr: 2001
+        }
+
+        $.ajax({
+            url: `${baseUrl}/medium`,
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(medium);
+        });
+    });
 });

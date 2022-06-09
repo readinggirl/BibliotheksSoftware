a17ca16654f4fe1e15f24482721c8d6f14fcdb24
Zwilli <mikeyeyey@gmail.com>
2022-06-09 19:19:31 +0200

    backend mediumDto properties hinzugefügt

--- BiblioBackend/src/main/java/Medien/MedienDataService.java
+++ BiblioBackend/src/main/java/Medien/MedienDataService.java
@@ -9,12 +9,23 @@ import java.util.List;
 public class MedienDataService {
     private HashMap<Integer, MediumDto> medien = new HashMap<>();
 
+
+    /*
+    *
+    private String medienArt;
+    private String systematik;
+    private String isbn;
+    * */
     public void createInitialMedien(){
         MediumDto m1 = new MediumDto();
         m1.setMedienNummer(123);
         m1.setJahr(2000);
         m1.setVerfasser("Ich, Du, Er, Sie, Es");
         m1.setSachtitel("Wir, Ihr, Sie");
+
+        m1.setMedienArt("art");
+        m1.setSystematik("sys");
+        m1.setIsbn("1293847");
 
         this.medien.put(m1.getMedienNummer(), m1);
 
@@ -24,6 +35,10 @@ public class MedienDataService {
         m2.setVerfasser("Coole Verfasser");
         m2.setSachtitel("Cooler Titel");
 
+        m2.setMedienArt("art2");
+        m2.setSystematik("sus");
+        m2.setIsbn("123412312");
+
         this.medien.put(m2.getMedienNummer(), m2);
 
         MediumDto m3 = new MediumDto();
@@ -31,6 +46,10 @@ public class MedienDataService {
         m3.setJahr(1997);
         m3.setVerfasser("fade Verfasser");
         m3.setSachtitel("fader Titel");
+
+        m3.setMedienArt("art3");
+        m3.setSystematik("amogus");
+        m3.setIsbn("6789670");
 
         this.medien.put(m3.getMedienNummer(), m3);
     }
--- BiblioBackend/src/main/java/Medien/MediumDto.java
+++ BiblioBackend/src/main/java/Medien/MediumDto.java
@@ -7,6 +7,9 @@ import org.springframework.data.annotation.Id;
 public class MediumDto {
     @Id
     private int medienNummer;
+    private String medienArt;
+    private String systematik;
+    private String isbn;
     private String sachtitel;
     private String verfasser;
     private int jahr;
